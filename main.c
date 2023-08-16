87b4d9e06618c017ff50e4a877f75836c5d78972
Casper Bené <48201144+cappe04@users.noreply.github.com>
2023-08-16 23:52:40 +0200

    calculate expression

--- main.c
+++ main.c
@@ -10,13 +10,13 @@
 static char TOKEN_STRING[] = "()-+/*";
 
 enum TokenType {
-    TOKEN_OPR, // 0     (
-    TOKEN_CPR, // 1     )
-    TOKEN_SUB, // 2     -
-    TOKEN_ADD, // 3     +
-    TOKEN_DIV, // 4     /
-    TOKEN_MUL, // 5     *
-    TOKEN_NUM, // 6     ex. 3.14
+    TOKEN_OPR = 0,  // 0    (
+    TOKEN_CPR,      // 1    )
+    TOKEN_SUB,      // 2    -
+    TOKEN_ADD,      // 3    +
+    TOKEN_DIV,      // 4    /
+    TOKEN_MUL,      // 5    *
+    TOKEN_NUM,      // 6    ex. 3.14
 };
 
 typedef struct _Token {
@@ -26,35 +26,38 @@ typedef struct _Token {
 
 void lexer(const char* string, Token* tokens, int* len_tokens);
 void parser(Token* tokens, int len_tokens, Token* postfix);
+double calculate(Token* postfix, int len_tokens);
 
+// ------ utility functions -------
 int token_typevalue(enum TokenType type);
 bool token_ge(Token a, Token b);
-
 void print_tokens(Token* tokens, int len);
+// --------------------------------
 
 int main(){
+    const char test_string[] = "(2.71 - 3.14 + 4) * (5.412 + 6/7)";
 
-    const char test_string[] = "((2.71 + 1.61) - 3.14 * (1 / 137)) + 1.6";
-    int test_len = strlen(test_string);
-
-    Token tokens[MAX_TOKENS];
     int len;
 
+    Token tokens[MAX_TOKENS];
     lexer(test_string, tokens, &len);
-    print_tokens(tokens, len);
-    printf("    %d\n", len);
+    printf("Tokenized expression:           ");
+    print_tokens(tokens, len); printf("\n");
 
     Token posfix[MAX_TOKENS];
-
+    printf("Expression in postfix notation: ");
     parser(tokens, len, posfix);
-    printf("\n");
+    print_tokens(posfix, len); printf("\n");
 
-    print_tokens(posfix, len);
+    double result = calculate(posfix, len);
+    printf("Calculated expression:          %f\n", result);
 
     return 0;
 }
 
 void lexer(const char* string, Token* tokens, int* len_tokens){
+    // Reads a string and creates tokens from numbers and operators
+    // Args: tokens as refrence, len_tokens as refrence
 
     char digits[MAX_STRING];
     memset(digits, 0, MAX_STRING);
@@ -64,40 +67,39 @@ void lexer(const char* string, Token* tokens, int* len_tokens){
     int i_string = 0;
 
     for(i_string; i_string<strlen(string); i_string++){
-
         // Ignore spaces
         if(string[i_string] == ' ') continue;;
-
         // If operator
         int i = 0;
         for(i; i < 6; i++){
             if(string[i_string] == TOKEN_STRING[i]){
                 if(len_digits > 0){
+                    // Create token from number buffer and flush buffer
                     tokens[num_token++] = (Token) { TOKEN_NUM, atof(digits) };
                     memset(digits, 0, MAX_STRING);
                     len_digits = 0;
                 }
-
+                // Create token from operator
                 tokens[num_token++] = (Token) { (enum TokenType) i, 0 };
                 break;
             }
         }
-
         // If not operator, add to digit buffer
         if(string[i_string] != TOKEN_STRING[i]){
             digits[len_digits++] = string[i_string];
         }
     }
-
     // If expression ends with digit, add it to tokens
     if(strlen(digits) > 0){
         tokens[num_token++] = (Token) { TOKEN_NUM, atof(digits) };
     }
-    
+
     *len_tokens = num_token;
 }
 
 void parser(Token* tokens, int len_tokens, Token* postfix){
+    // Create postfix notaion from given tokens
+    // Args: postfix by refrence
 
     Token stack[MAX_TOKENS];
     int len_stack = 0;
@@ -106,19 +108,16 @@ void parser(Token* tokens, int len_tokens, Token* postfix){
     for(int i_token = 0; i_token < len_tokens; i_token++){
 
         Token token = tokens[i_token];
-
         // Add to postfix if number
         if(token.type == TOKEN_NUM){
             postfix[len_postfix++] = token;
             continue;
         }
-
         // If token is "("
         if(token.type == TOKEN_OPR){
             stack[len_stack++] = token;
             continue;
         }
-
         // If token is ")" add to postfix until "("
         if(token.type == TOKEN_CPR){
             while (len_stack > 0 && stack[len_stack - 1].type != TOKEN_OPR){
@@ -128,7 +127,6 @@ void parser(Token* tokens, int len_tokens, Token* postfix){
             postfix[len_postfix++] = token;
             continue;
         }
-        
         // If new operator is < stack top empty stack until 
         // new operator is > stack top, than add to stack
         while(len_stack > 0 && token_ge(stack[len_stack - 1], token)){
@@ -136,13 +134,60 @@ void parser(Token* tokens, int len_tokens, Token* postfix){
         }
         stack[len_stack++] = token;
     }
-
     // Empty stack
     while(len_stack > 0){
         postfix[len_postfix++] = stack[--len_stack];
     }
 };
 
+double calculate(Token* postfix, int len_tokens){
+    // Calculates posfix expression
+
+    double stack[MAX_TOKENS];
+    int len_stack = 0;
+
+    for(int i_postfix = 0; i_postfix<len_tokens; i_postfix++){
+        Token token = postfix[i_postfix];
+        // Ignore parentheses
+        if(token_typevalue(token.type) == 0) continue;
+        // If token is number add to stack
+        if(token.type == TOKEN_NUM){
+            stack[len_stack++] = token.value;
+            continue;
+        }
+        // If token is oprator, twice from stack, do operation
+        // And append back to stack
+        // It's equvelent to reduce(op, stack) in other language
+        double b = stack[--len_stack];
+        double a = stack[--len_stack];
+
+        switch (token.type){
+        case TOKEN_ADD:
+            stack[len_stack++] = a + b;
+            break;
+        
+        case TOKEN_SUB:
+            stack[len_stack++] = a - b;
+            break;
+        
+        case TOKEN_DIV:
+            stack[len_stack++] = a / b;
+            break;
+        
+        case TOKEN_MUL:
+            stack[len_stack++] = a * b;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    // Stack has been reduced to only one elemnt
+    // Return that element
+    return stack[0];
+};
+
+// ------ utility functions -------
 bool token_ge(Token a, Token b){
     return token_typevalue(a.type) >= token_typevalue(b.type);
 }
@@ -177,3 +222,4 @@ void print_tokens(Token* tokens, int len){
         };
     }
 };
+// --------------------------------
